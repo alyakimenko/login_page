543216c31986ae675dd5f20532a90ae381b71375
alyakimenko <yakimenkocntu@gmail.com>
2019-02-18 12:17:21 +0200

    Fix bug

--- login_page/__init__.py
+++ login_page/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.secret_key = b'\xbb\xe2\x91\xa5\xd7\xc7\x7fJ\x99\xe6\x9e\xdc\x91\xc7\xea\x9a'
+    app.secret_key = "here is my secret key"
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
     db.init_app(app)
